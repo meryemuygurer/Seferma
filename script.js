1910f9fac6d3e06bd4fa2dda57b100c44b6d48de
Meryem Uygurer <hmuygurer@outlook.com>
2024-01-08 01:58:30 +0300

    Home Page Update And First Api Events

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ window.onload = function () {
 
 // Seçenekleri değiştiren JavaScript fonksiyonu
 function selectOption(optionNumber) {
-    console.log("deneme");
     // Tüm seçeneklerin rengini gri yap
     for (let i = 1; i <= 4; i++) {
         let eleman = document.getElementById('option' + i)
@@ -21,6 +20,50 @@ function selectOption(optionNumber) {
     // Tıklanan seçeneğin rengini siyah yap
     let eleman2 = document.getElementById('option' + optionNumber)
     eleman2.querySelector("h3").style.color = '#c90005';
-
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var tarihInput = document.getElementById("tarihSec");
+
+    // Bugünün tarihini al
+    var bugununTarihi = new Date();
+
+    // Tarihi "YYYY-MM-DD" formatına dönüştür
+    var formatliTarih = getFormattedDate(bugununTarihi);
+
+    // Tarih input'unun değerini güncelle
+    tarihInput.value = formatliTarih;
+});
+
+function getFormattedDate(date) {
+    var year = date.getFullYear();
+
+    var month = (date.getMonth() + 1).toString();
+    month = month.length === 1 ? '0' + month : month;
+
+    var day = date.getDate().toString();
+    day = day.length === 1 ? '0' + day : day;
+
+    return year + '-' + month + '-' + day;
+}
+
+var adresInputList = document.querySelectorAll(".adresInput");
+
+function initMap() {
+    adresInputList.forEach(function (adresInput) {
+        var autocomplete = new google.maps.places.Autocomplete(adresInput);
+
+        autocomplete.addListener('place_changed', function () {
+            var place = autocomplete.getPlace();
+            if (place.geometry) {
+                console.log(adresInput.id + ' için Seçilen Adres:', place.formatted_address);
+                // İstediğiniz başka işlemleri burada yapabilirsiniz
+            } else {
+                console.error(adresInput.id + ' için geçerli bir adres seçilmedi.');
+            }
+        });
+    });
+}
+
 
